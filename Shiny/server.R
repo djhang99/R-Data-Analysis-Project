0fa746dfe391eba29bf495bf42d03cabbe15bb90
djhang <david.jhang@gmail.com>
2021-10-30 16:08:07 -0400

    finished almost all charts.. going to attempt linear regression for my last graphs

--- Shiny/server.R
+++ Shiny/server.R
@@ -4,9 +4,9 @@ library(ggplot2)
 
 
 server <- shinyServer(function(input, output, session) {
+  
   # set working directory and load csv file
-  knitr::opts_chunk$set(root.dir = normalizePath("C:/Users/David Jhang/Desktop/NYC Data Academy/Project 2"))
-  data <- read.csv(file = "/Project 2/df.csv")
+  data <- read.csv(file = "./df.csv")
   
   colscale <- c(semantic_palette[["red"]], semantic_palette[["green"]], semantic_palette[["blue"]])
   mtcars$am <- factor(data$num_suicides,levels=c(0,1),
