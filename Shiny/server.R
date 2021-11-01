015d1b80ec4c2f7b04573b57f27e077af45c4d43
djhang <david.jhang@gmail.com>
2021-10-31 21:21:52 -0400

    finished everything.. maybe one last review but shiny app finished

--- Shiny/server.R
+++ Shiny/server.R
@@ -27,8 +27,30 @@ server <- shinyServer(function(input, output, session) {
       group_by(year, age) %>%
       summarise(yearly_suicides = sum(num_suicides), gdp_cap = mean(gdp_per_capita))
   })
-  
+
+  #creating subset dataframe for age graph
+  chart4 <- reactive({
+    req(input$sel_gdp)
     
+    data1 = data %>%
+    group_by(country, year) %>%
+    summarise(gdp_per_capita = mean(gdp_per_capita), total_suicides=round(sum(num_suicides),2))
+    
+    gdp_df = data1 %>% 
+      filter(country %in% input$sel_gdp) %>%
+      group_by(year) 
+  })  
+
+  #creating subset dataframe for gender graph
+  chart5 <- reactive({
+    req(input$sel_gender)
+    
+    gender_df = data %>% 
+      filter(country %in% input$sel_gender) %>%
+      group_by(year, sex) %>%
+      summarise(yearly_suicides = sum(num_suicides), yearly_pop= sum(population), gdp_cap=mean(gdp_per_capita))
+  })
+  
   
   # Interactive Chart for "country graph" Tab 
   output$country_plot <- renderPlot({
@@ -50,7 +72,22 @@ server <- shinyServer(function(input, output, session) {
                   position = position_jitter(0.21)) +
       labs(title = "Yearly Suicides by Age Group", x = "Age Group", y = "# of suicides (yearly)") +
       theme_classic()  
+  })
+  
+  # Interactive Chart for "gdp graph" Tab 
+  output$gdp_plot <- renderPlot({
+    ggplot(chart4(), aes(x = total_suicides, y = gdp_per_capita, color= total_suicides)) +
+      geom_point() +
+      geom_line() +
+      labs(title = "Yearly # of Suicides vs Yearly GDP", x = "Yearly Suicides", y = "GDP (per capita)") +
+      theme(legend.position = "none")
+  })
 
+  # Interactive Chart for "gender graph" Tab 
+  output$gender_plot <- renderPlot({
+    ggplot(chart5(), aes(x=year, y=yearly_suicides, fill=sex)) + 
+      geom_bar(stat="identity", position=position_dodge()) +
+      labs(title = "Male vs Female Breakdown", x = "Year", y = "# of suicides")
   })
   
   
